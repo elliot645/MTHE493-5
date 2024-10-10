d637e58598ce62422c5b58a1f85f27d70f813aaa
Elliot Thoburn <80990165+elliot645@users.noreply.github.com>
2024-10-10 08:19:19 -0400

    test

--- model/testing.py
+++ model/testing.py
@@ -1,3 +1,5 @@
-from graphutils import create_random_graph
+#from graphutils import create_random_graph
 
-random_graph = create_random_graph(num_nodes = 5, num_edges = 15)
+#random_graph = create_random_graph(num_nodes = 5, num_edges = 15)
+
+print("hello world")
