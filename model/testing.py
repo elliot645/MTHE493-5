cbfb2f7cbe8d364df34ac941cf78d3a749d31f5d
ELLIOT THOBURN <ELLIOT.WELDON.THOBURN@gmail.com>
2024-10-21 12:00:20 -0400

    graph and node class set up

--- model/testing.py
+++ model/testing.py
@@ -1,5 +1,7 @@
-#from graphutils import create_random_graph
+# import sys
+# sys.path.insert(1, 'utils')
+# from graphutils import create_random_graph, print_graph
 
-#random_graph = create_random_graph(num_nodes = 5, num_edges = 15)
+# random_graph = create_random_graph(num_nodes = 5, num_edges = 15)
 
-print("hello world")
+# print_graph(random_graph,1)
