a31e8c60d492085fd4dc07d6cd5c4fe2cecce183
Owen <owenkeddie1@gmail.com>
2024-11-12 12:40:08 -0500

    uhbug

--- utils/polya_process.py
+++ utils/polya_process.py
@@ -39,32 +39,42 @@ def polya(graph, reinforce, n,state_filter=None):
             results[node_id].append(data)
     return graph, results
 
-def polya_reinforce_by_node(graph):
-   results = []
-   for node in graph.nodes:
-       r = node.red 
-       b = node.blue
+def polya_reinforce_by_node(graph, n, state_filter=None):
+    if state_filter is not None:
+        results = {node_id: [] for node_id in graph.nodes if graph.nodes[node_id].state == state_filter}
+    else:
+        results = {node_id: [] for node_id in graph.nodes}
 
-       for neighbour in node.neighbours:
-           t += (neighbour.red + neighbour.blue)
-           r += neighbour.red
-           b += neighbour.blue
+    for i in range(0,n):
 
-           t = r + b
-           prob_r = r/t
+        for node_id,node in graph.nodes.items():
+            if state_filter is not None:
+                if node.state != state_filter:
+                    continue
 
-       if random.random() < prob_r:
-           choose = 'r'
-       else:
-           choose = 'b'
+            r = node.red 
+            b = node.blue       
+            
+        
+            for neighbour in node.neighbours:
+                r += graph.nodes[neighbour].red
+                b += graph.nodes[neighbour].blue
+            t = r + b
+            prob_r = r/t
 
-       if choose == 'r':
-           node.r += node.reinforcement_parameter
-       else:
-           node.b += node.reinforcement_parameter
+            if random.random() < prob_r:
+                choose = 'r'
+            else:
+                choose = 'b'
 
-       data = [choose,node.red + node.blue + node.reinforcement_parameter,node.red/(node.red + node.blue + node.reinforcement_parameter)]
-       results.append(data) 
-   return      
+            if choose == 'r':
+                node.red += node.reinforcement_parameter
+            else:
+                node.blue += node.reinforcement_parameter
 
-#each node has a list of its neighbours from adjacency matrix
+            data = {'choose':choose,
+                    'total':node.red + node.blue,
+                    'red ratio': node.red/(node.red + node.blue)
+            }
+            results[node_id].append(data)
+    return graph, results      
