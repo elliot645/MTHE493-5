7be229a87113f4c0854ca0153f0c84bdcb8d6fb0
Anna <20asw@queensu.ca>
2025-03-04 11:09:28 -0500

    tweaked algorithms

--- approx_main.py
+++ approx_main.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import scipy.integrate as integrate
 from scipy.stats import beta
+import pandas as pd
 
 # plot county opinion over time for given state
 def plot_opinions(statedict, votesdict, state):
@@ -31,21 +32,67 @@ def plot_beta_pdf(R, B, deltas):
     plt.xlabel("ratio of red u")
     plt.ylabel("f(U=u)")
     plt.legend()
+    return
+
+def get_data(fipsdict, votesdict, startyear, endyear):
+    data = {}
+    for state in fipsdict:
+        for county in fipsdict[state]:
+            r0 = votesdict[startyear][county]["REPUBLICAN"]
+            b0 = votesdict[startyear][county]["DEMOCRAT"]
+            r1 = votesdict[endyear][county]["REPUBLICAN"]
+            b1 = votesdict[endyear][county]["DEMOCRAT"]
+            pop = r0+b0
+            opinion = r0/(r0+b0)
+            change = abs((r1/(r1+b1))-(r0/(r0+b0)))
+            data[county] = {
+                "Population" : pop,
+                "Opinion" : opinion,
+                "change" : change
+            }
+    return data
+
+# pass in dict like {fips:{data:int, change:int}}
+def plot_data(data, startyear, endyear, key):
+    xvalues = []
+    yvalues = []
+    for county in data:
+        xvalues.append(data[county][key])
+        yvalues.append(data[county]["change"])
+    plt.scatter(xvalues, yvalues, s=0.5)
+    plt.xlabel("Initial " + key)
+    plt.ylabel("Change in Opinion")
+    plt.title(startyear + "-" + endyear)
     plt.show()
-        
-# plot error over time 
-# delta is a node attribute
-def plot_error(graph, T=1000):
-    pass
+    return
+
 
 if __name__ == "__main__":
 
     fips_path = r"data\statefips.json"
     votes_path = r"data\votingdata.json"
+    adj_path = r"data\county_adjacency.csv"
 
-    # plot error for delta=1
+    state = "NH"
+    startyear = 2000
+    endyear = 2004
+    timesteps = 10000
 
-    # plot error for delta = f(change in U)
+    fipsdict = get_fipsdict(data_path, fips_sheet, state)    # Nodes
+    neighbours = get_adjacency_dict(adj_path, fipsdict)      # Edges
+    network = Graph()                                        #
+    network.set_topology(fipsdict, neighbours)
+    vdf = get_df(data_path, votes_sheet)                     #
+    startvotes = get_votes(vdf, fipsdict, startyear, state)
+    endvotes = get_votes(vdf, fipsdict, endyear, state)
+
+    # initialize nodes with R, B, delta
+
+    # run Polya process
+    
+
+
+
 
     """
     Observations
