2193351ae3001915843e8e42532a916f5b5e94ec
Anna <20asw@queensu.ca>
2025-04-08 16:15:09 -0400

    preparing for submission

--- approx_main.py
+++ approx_main.py
@@ -7,13 +7,12 @@ import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
 
-
     # filepaths
-    fips_path = r'data\statefips.json'    # ignore AK, HI
-    adj_path = r'data\countyadj.json'
-    votes_path = r'data\votingdata.json'
-    po_path = r'data\state_PO.json'
-    results_path = r'data\results.xlsx'
+    fips_path = 'data/statefips.json'    # ignore AK, HI
+    adj_path = 'data/countyadj.json'
+    votes_path = 'data/votingdata.json'
+    po_path = 'data/state_PO.json'
+    out_path = 'data/mapresults.xlsx'
 
     # set up dicts
     fipsdict = get_dict_from_json(fips_path)
@@ -79,48 +78,11 @@ if __name__ == "__main__":
         print(state, 'complete.')
 
     df = pd.DataFrame.from_dict(results, orient='index')
-    out_path = 'data/mapresults.xlsx'
     df.to_excel(out_path)
 
     
     
-    # dataframes = {}
-    # for endyear in ['2016']:
-    #     results = {}
-    #     for state in graphs:
-    #         network = graphs[state]
-    #         # get data that won't be altered in trials
-    #         for node in network:
-    #             endr = votesdict[endyear][node.id]['REPUBLICAN']
-    #             endb = votesdict[endyear][node.id]['DEMOCRAT']
-    #             node.real_ratio = endr/(endr+endb)
-    #             node.delta = 1
-    #         # get average error across all trials
-    #         total_error = 0
-    #         for trial in range(0,trials):
-    #             # reset initial conditions
-    #             for node in network:
-    #                 node.red = votesdict[startyear][node.id]['REPUBLICAN']
-    #                 node.blue = votesdict[startyear][node.id]['DEMOCRAT']
-    #                 node.pop = node.red + node.blue
-    #             # perform polya process
-    #             trial_error, total_pop = network_polya(network, timesteps)
-    #             total_error += trial_error
-    #         # add to results
-    #         avg_error = total_error/trials
-    #         results[podict[state]] = {
-    #             'Avg Error' : round(avg_error*100,2),
-    #             'Total Votes' : total_pop
-    #         }
-    #         print(state, 'complete.')
-    #     # create dataframe from results for this year
-    #     dataframes[endyear] = pd.DataFrame.from_dict(results, orient='index')
-    #     print(endyear, 'complete.')
 
-    # # write to excel sheet
-    # with pd.ExcelWriter(results_path) as writer:
-    #     for endyear in dataframes:
-    #         dataframes[endyear].to_excel(writer, sheet_name=startyear+'-'+endyear)
 
     
 
--- utils/approx_utils.py
+++ utils/approx_utils.py
@@ -89,6 +89,44 @@ def plot_beta_pmf(R, B, delta):
 # DEPRECATED
 #=============================================================
 
+# dataframes = {}
+# for endyear in ['2016']:
+#     results = {}
+#     for state in graphs:
+#         network = graphs[state]
+#         # get data that won't be altered in trials
+#         for node in network:
+#             endr = votesdict[endyear][node.id]['REPUBLICAN']
+#             endb = votesdict[endyear][node.id]['DEMOCRAT']
+#             node.real_ratio = endr/(endr+endb)
+#             node.delta = 1
+#         # get average error across all trials
+#         total_error = 0
+#         for trial in range(0,trials):
+#             # reset initial conditions
+#             for node in network:
+#                 node.red = votesdict[startyear][node.id]['REPUBLICAN']
+#                 node.blue = votesdict[startyear][node.id]['DEMOCRAT']
+#                 node.pop = node.red + node.blue
+#             # perform polya process
+#             trial_error, total_pop = network_polya(network, timesteps)
+#             total_error += trial_error
+#         # add to results
+#         avg_error = total_error/trials
+#         results[podict[state]] = {
+#             'Avg Error' : round(avg_error*100,2),
+#             'Total Votes' : total_pop
+#         }
+#         print(state, 'complete.')
+#     # create dataframe from results for this year
+#     dataframes[endyear] = pd.DataFrame.from_dict(results, orient='index')
+#     print(endyear, 'complete.')
+
+# # write to excel sheet
+# with pd.ExcelWriter(results_path) as writer:
+#     for endyear in dataframes:
+#         dataframes[endyear].to_excel(writer, sheet_name=startyear+'-'+endyear)
+
 # # plot county opinion over time for given state
 # def plot_opinions(statedict, votesdict, state):
 #     years = [2000, 2004, 2008, 2012, 2016, 2020]
